5da751703f8d3351b31385ec1f2e9e8e2bafc459
Dev07 <dev07@192.168.0.102>
2019-01-01 21:02:49 +0200

    Join

--- server/server.js
+++ server/server.js
@@ -9,7 +9,16 @@ var io = socketIO(server);
 const port = process.env.PORT || 3000;
 io.on('connection',(socket)=>{
 
+    socket.emit('connect','connection success')
 console.log(socket.id);
+
+
+io.on('userJoin',(params,callback)=>{
+    if(params.name=='jihad')
+    {
+        callback('success');
+    }
+})
 });
 
 
